cb1715c552ff7a16e7da2956f0e3cfcb3741db92
Kleiton Pimenta <kleitonpsld@hotmail.com>
2021-10-17 19:54:41 -0300

    altera jenkinsfile

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,6 +3,12 @@
 def MAVEN_VERSION="maven-3.6.3"
 
 pipeline {
+    environment {
+        imagewebgoat = "kleitonpimenta/webgoat-8.0:1.0.0"
+        imagewebwolf = "kleitonpimenta/webwolf-8.0:1.0.0"
+        registryCredential = 'kleitonpimenta'
+        dockerImage = ''
+    }
     agent any
 
     stages {
@@ -33,15 +39,73 @@ pipeline {
                 echo 'Testing...'
             }
         }
-        stage('Deploy') {
+        stage('Building image') {
             steps {
-                echo 'Deploying....'
+                echo 'Building image....'
                 script {
-                    sh "docker build -f webgoat-server/Dockerfile webgoat-server/. --tag webgoat-8.0:1.0.0"
-                    sh "docker build -f webwolf/Dockerfile webwolf/. --tag webwolf-8.0:1.0.0"
-                    sh "docker-compose up -d --force-recreate"
+                    sh "cd webgoat-server"
+                    dockerImage = docker.build imagewebgoat
+                    sh "cd ../webwolf/"
+                    dockerImage = docker.build imagewebwolf
                 }
             }
         }
+        stage('Deploy Image') {
+          steps{
+             script {
+                    docker.withRegistry( ":$PASSWORD", registryCredential ) {
+                    dockerImage.push(imagewebgoat)
+                    dockerImage.push(imagewebwolf)
+                    }
+                }
+            }
+        }
+        stage('Pull imagem') {
+            steps {
+                script{
+                    echo 'Pull imagem WebGoat'
+		            def urlImage = "http://10.138.0.2:2376/images/create?fromImage=kleitonpimenta/webgoat-8.0:1.0.0";
+                    def response = httpRequest url:"${urlImage}", httpMode:'POST', acceptType: 'APPLICATION_JSON', validResponseCodes:"200"
+                    println("Status: " + response.status)
+                    def pretty_json = writeJSON( returnText: true, json: response.content)
+                    println pretty_json
+
+                    echo 'Pull imagem WebWolf'
+		            def urlImage = "http://10.138.0.2:2376/images/create?fromImage=kleitonpimenta/webwolf-8.0:1.0.0";
+                    def response = httpRequest url:"${urlImage}", httpMode:'POST', acceptType: 'APPLICATION_JSON', validResponseCodes:"200"
+                    println("Status: " + response.status)
+                    def pretty_json = writeJSON( returnText: true, json: response.content)
+                    println pretty_json
+                    
+                }
+            }
+        }
+        stage('Criar container') {
+            steps {
+                script{
+                    configFileProvider([configFile(fileId: '0d7d58cc-3e47-4be9-af81-f99b951f7392', targetLocation: 'container.json')]) {
+
+                        
+                    	echo 'Criando container webgoat'
+			            def url = "http://10.138.0.2:2376/containers/webgoat?force=true"
+                        def response = sh(script: "curl -v -X DELETE $url", returnStdout: true).trim()
+                        echo response
+
+                        url = "http://10.138.0.2:2376/containers/create?name=webgoat"
+                        response = sh(script: "curl -v -X POST -H 'Content-Type: application/json' -d @container.json -s $url", returnStdout: true).trim()
+                        echo response
+
+                    	echo 'Criando container webwolf'
+			            def url = "http://10.138.0.2:2376/containers/webwolf?force=true"
+                        def response = sh(script: "curl -v -X DELETE $url", returnStdout: true).trim()
+                        echo response
+
+                        url = "http://10.138.0.2:2376/containers/create?name=webwolf"
+                        response = sh(script: "curl -v -X POST -H 'Content-Type: application/json' -d @container.json -s $url", returnStdout: true).trim()
+                        echo response
+                    }
+                }
+            }
+        } 
     }
 }
